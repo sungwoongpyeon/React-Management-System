4bb24e36fbea7047f402339a65a13001312cf3e9
sungwoong <tjddnd523@gmail.com>
2019-11-17 23:32:42 -0500

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-      
+      customers: [
+        {
+          'id': 1,
+          'image': 'https://placeimg.com/64/64/1',
+          'name': 'Sungwoong Pyeon',
+          'birthday': '930621',
+          'gender': 'Male',
+          'job': 'Student',
+        },
+        {
+          'id': 2,
+          'image': 'https://placeimg.com/64/64/2',
+          'name': 'Yago Santos',
+          'birthday': '920505',
+          'gender': 'Male',
+          'job': 'Student',
+        },
+        {
+          'id': 3,
+          'image': 'https://placeimg.com/64/64/3',
+          'name': 'Alex',
+          'birthday': '900404',
+          'gender': 'Male',
+          'job': 'Student',
+        }
+      ]
     }
   }
-  render(){
-    return(
-      <div className="gray-background">
-          {/* <img src={logo} lat="logo" /> */}
-          <h2>Let's develop management system!</h2> 
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+
       </div>
     );
   }
 }
 
 export default App;
+
