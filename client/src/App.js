6ff3d8770ff647582de019afd4a0fd24cc4c53df
sungwoong <tjddnd523@gmail.com>
2019-11-18 01:16:11 -0500

    Add Get Customers REST/JSON API Module

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,13 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
 
-const styles = theme =>({
-  root:{
+const styles = theme => ({
+  root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: 'auto',
   },
   table: {
@@ -24,37 +24,24 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      customers: [
-        {
-          'id': 1,
-          'image': 'https://placeimg.com/64/64/1',
-          'name': 'Sungwoong Pyeon',
-          'birthday': '930621',
-          'gender': 'Male',
-          'job': 'Student',
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/2',
-          'name': 'Yago Santos',
-          'birthday': '920505',
-          'gender': 'Male',
-          'job': 'Student',
-        },
-        {
-          'id': 3,
-          'image': 'https://placeimg.com/64/64/3',
-          'name': 'Alex',
-          'birthday': '900404',
-          'gender': 'Male',
-          'job': 'Student',
-        }
-      ]
+      customers: '',
     }
   }
 
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
-    const {classes} = this.props;
+    const { classes } = this.props;
     return (
       <Paper className={classes.root}>
         <Table className={classes.table}>
@@ -70,7 +57,7 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {
-              this.state.customers.map(c => {
+              this.state.customers ? this.state.customers.map(c => {
                 return (
                   <Customer
                     key={c.id}
@@ -82,7 +69,7 @@ class App extends Component {
                     job={c.job}
                   />
                 );
-              })
+              }) : ""
             }
           </TableBody>
         </Table>
--- server.js
+++ server.js
@@ -6,8 +6,49 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: "Hello Express!"});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://placeimg.com/64/64/1',
+            'name': 'Sungwoong Pyeon',
+            'birthday': '930621',
+            'gender': 'Male',
+            'job': 'Student',
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name': 'Yago Santos',
+            'birthday': '920505',
+            'gender': 'Male',
+            'job': 'Student',
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name': 'Alex',
+            'birthday': '900404',
+            'gender': 'Male',
+            'job': 'Student',
+        },
+        {
+            'id': 4,
+            'image': 'https://placeimg.com/64/64/4',
+            'name': 'Ceagan',
+            'birthday': '991105',
+            'gender': 'Male',
+            'job': 'Student',
+        },
+        {
+            'id': 5,
+            'image': 'https://placeimg.com/64/64/5',
+            'name': 'Ji-Hee',
+            'birthday': '890905',
+            'gender': 'Female',
+            'job': 'Student',
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
